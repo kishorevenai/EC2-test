2c2f56131772e6bb3e42299254afcace833107a6
kishorevenai <kishorevenai@gmail.com>
2025-07-11 00:03:50 +0530

    -

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
